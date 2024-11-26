17e5fa713c81b9c6d2d9b472c74c244d6a1a713a
Patchouli <self@patchouli.sh>
2024-11-25 19:20:46 -0500

    Add unit test initialization logic

---
+++ source/Lambda.Tests/Cloud/LambdaCloudServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lambda.Cloud
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestClass]
+	public sealed partial class LambdaCloudServiceTests
+	{
+		private ILambdaCloudService lambdaCloud;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[TestInitialize]
+		public void Initialize()
+		{
+			this.lambdaCloud =
+				new LambdaCloudService(
+					Environment.GetEnvironmentVariable(
+						"LAMBDA_API_KEY"));
+		}
+	}
+}
