0b1038dd84ab592b579654d8f2236b457a917139
Patchouli <self@patchouli.sh>
2024-11-25 19:20:18 -0500

    Add DI extensions to Lambda Cloud service

---
+++ source/Lambda.Extensions.DependencyInjection/Cloud/LambdaCloudServiceOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lambda.Cloud
+{
+	/// <summary>
+	/// Represents the options when registering an instance of the <see cref="LambdaCloudService"/>
+	/// class with an <see cref="IServiceCollection"/>.
+	/// </summary>
+	public sealed class LambdaCloudServiceOptions
+	{
+		/// <summary>
+		/// Gets or sets the base URL to use.
+		/// </summary>
+		public Uri BaseUrl { get; set; } =
+			LambdaCloudService.DefaultBaseUrl;
+
+		/// <summary>
+		/// Gets or sets the API key to use.
+		/// </summary>
+		public string ApiKey { get; set; }
+	}
+}
---
+++ source/Lambda.Extensions.DependencyInjection/Cloud/ServiceCollectionExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lambda.Cloud
+{
+	/// <summary>
+	/// Contains extension methods for
+	/// <see cref="IServiceCollection"/>.
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// The name to use for the named client.
+		/// </summary>
+		private const string LambdaCloudHttpClientName =
+			"lambda-cloud";
+
+		/// <summary>
+		/// Registers an instance of the <see cref="LambdaCloudService"/>
+		/// class with an <see cref="IServiceCollection"/>
+		/// using a named <see cref="HttpClient"/>.
+		/// </summary>
+		/// <param name="services">The service collection to register with.</param>
+		/// <param name="optionsCallback">The callback to configure the options for the instance with.</param>
+		/// <returns>An <see cref="IHttpClientBuilder"/> that can be used to configure the named client.</returns>
+		public static IHttpClientBuilder AddLambdaCloudHttpClient(
+			this IServiceCollection services,
+			Action<LambdaCloudServiceOptions> optionsCallback = default)
+		{
+			Guard.NotNull(
+				services,
+				nameof(services));
+
+			var options =
+				new LambdaCloudServiceOptions();
+
+			optionsCallback?
+				.Invoke(
+					options);
+
+			return services
+				.AddTransient<LambdaCloudDelegatingHandler>()
+				.AddHttpClient<ILambdaCloudService, LambdaCloudService>(
+					LambdaCloudHttpClientName,
+					(httpClient) =>
+					{
+						// Configure the base URL for the client.
+						httpClient.BaseAddress =
+							options.BaseUrl;
+
+						// Configure the default request headers for the client.
+						httpClient.DefaultRequestHeaders.Add(
+							$"Authorization",
+							$"Bearer {options.ApiKey}");
+					})
+				.AddHttpMessageHandler<LambdaCloudDelegatingHandler>();
+		}
+	}
+}
